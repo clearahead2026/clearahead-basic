89255c477cd9f2d3c2dd6ceea5da75df92968d00
clearahead2026 <clearahead2026@gmail.com>
2026-02-13 10:43:04 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -17,15 +17,17 @@ export default defineConfig({
         display: "standalone",
         orientation: "portrait",
         start_url: "/",
-        icons: [
-  {
-    src: "/pwa-192x192.png",
-    sizes: "192x192",
-    type: "image/png"
-  },
-  {
-    src: "/pwa-512x512.png",
-    sizes: "512x512",
-    type: "image/png"
-  }
-],
+                icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
